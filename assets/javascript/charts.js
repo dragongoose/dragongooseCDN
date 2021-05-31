3e8bf463e0509a404c423d7752c8fffdbafd963e
Dragon <tylerjseverino@gmail.com>
2021-05-30 21:32:21 -0400

    change styling

--- assets/javascript/charts.js
+++ assets/javascript/charts.js
@@ -1,4 +1,8 @@
 var ctx = document.getElementById('myChart').getContext('2d');
+
+Chart.defaults.global.defaultFontColor = "#df49a6";
+//Chart.defaults.global.defaultFontStyle = 'Bold'
+
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -6,44 +10,50 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1
         }]
     },
     options: {
-        responsive: true,
-        gridLines: {
-            display: false
-         },
         scales: {
             y: {
                 beginAtZero: true
             },
+            pointLabels :{
+                fontStyle: "bold",
+            },     
             xAxes: [{
                 gridLines: {
                     display:false
+                },
+                ticks: {
+                    fontFamily: "Open Sans",
+                    fontStyle: "bold"
                 }
             }],
             yAxes: [{
                 gridLines: {
                     display:false
+                },
+                ticks: {
+                    fontFamily: "Open Sans",
+                    fontStyle: "bold"
                 }   
-            }]
-        }
+            }],
+            plugins: {
+                legend: {
+                    labels: {
+                        // This more specific font property overrides the global property
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        },
+        legend: {
+            display: false
+         }
     }
 });
