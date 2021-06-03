b3b5f4a04af05197c12b2f1a40ef6c02182d09ef
dragonshadow14 <tylerjseverino@gmail.com>
2021-06-03 13:37:59 -0400

    Change chart design

--- assets/javascript/charts.js
+++ assets/javascript/charts.js
@@ -6,54 +6,70 @@ Chart.defaults.global.defaultFontColor = "#df49a6";
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec'],
-        datasets: [{
-            label: 'Size in MB',
-            data: [100,150,200,500],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        }]
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+        ],
+        datasets: [
+            {
+                data: [0, 60, 100, 150, 300, 340, 400],
+                backgroundColor: "#fa14e7",
+                borderColor: "#df49a6",
+                pointBackgroundColor: "#db34d3",
+                pointBorderColor: "#ab4aa6",
+                label: "Size (MB)",
+            },
+        ]
     },
     options: {
+        responsive: true,
+        layout: { padding: { top: 15 } },
         scales: {
-            y: {
-                beginAtZero: true
-            },
-            pointLabels :{
-                fontStyle: "bold",
-            },     
-            xAxes: [{
-                gridLines: {
-                    display:true
-                },
-                ticks: {
+            xAxes: [
+                {
+                    gridLines: { display: false },
                     fontFamily: "Open Sans",
                     fontStyle: "bold"
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    display:true
                 },
-                ticks: {
+
+            ],
+
+            yAxes: [
+                {
+                    gridLines: { display: false },
                     fontFamily: "Open Sans",
                     fontStyle: "bold"
-                }   
-            }],
-            plugins: {
-                legend: {
-                    labels: {
-                        // This more specific font property overrides the global property
-                        font: {
-                            size: 14
-                        }
-                    }
-                }
-            }
+                },
+            ],
         },
-        legend: {
-            display: false
-         }
+        plugins: {
+            datalabels: { display: false },
+        },
+        legend: { display: false },
+        elements: {
+            arc: {},
+            point: {},
+            line: { tension: 0.2, fill: false },
+            rectangle: {},
+        },
+        tooltips: {
+            intersect: false,
+            backgroundColor: "#141418",
+            titleFontColor: "#df49a6",
+            titleFontFamily: "Merriweather Sans",
+            titleFontSize: 14,
+            bodyFontColor: "#df49a6",
+            bodyFontFamily: "Lato",
+            cornerRadius: 3,
+        },
+        hover: {
+            mode: "nearest",
+            animationDuration: 300,
+        },
     }
 });
