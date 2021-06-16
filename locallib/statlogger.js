2a1b85963d86e4c0b7d8bb99ff30774e0751fb12
Dragon <tylerjseverino@gmail.com>
2021-06-16 17:03:52 -0400

    change how the stat logger works

--- locallib/statlogger.js
+++ locallib/statlogger.js
@@ -7,8 +7,9 @@ var dir = `../stats/`;
 
 
 //if stats directory doesnt exists add
-if (!fs.existsSync(dir)){
+if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
+    fs.writeFileSync(`${dir}stats.stats`, '[]');
 }
 
 var before = new Date().getTime() //get current time
@@ -20,22 +21,35 @@ console.log('Logging stats.')
 var files = stats.totalFiles()
 
 stats.totalSize.then(size => {
-    console.log(size)
+
+    var statsarray;
+
     var data = {
         date: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
         chartdate: date.slice(0, -17),
         totalSize: size,
         totalFiles: files.totalFiles.length,
     }
-    
-    fs.writeFile(`${dir}${filenamedate}.stats`, JSON.stringify(data), (err) => {
-        if(err) return console.log('ERROR WHILE SAVING STATS:')
+
+    var statsarray = fs.readFileSync(`${dir}stats.stats`, {encoding:'utf8', flag:'r'});
+    var statsarray = JSON.parse(statsarray);
+
+    if(statsarray.length == 12){
+        statsarray.pop();
+    };
+
+    statsarray.push(data);
+
+    fs.writeFile(`${dir}stats.stats`, JSON.stringify(statsarray), (err) => {
+        if (err) {throw err}
         var after = new Date().getTime() - before
         console.log(`Done! took ${after}ms`);
         exit();
     })
+
+
 })
-.catch(err => console.log(`Error saving stats, ${err}`))
+    .catch(err => console.log(`Error saving stats, ${err}`))
 
 
 
